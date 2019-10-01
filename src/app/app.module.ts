24a1a52c5c2f9000ec7211c7eeb6e306268d9b26
FedeSchmidt <fschmidt.10@hotmail.com>
2019-10-01 11:05:29 -0300

    linear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { LinearComponent } from './linear/linear.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: LinearComponent },
+  { path: 'linear', component: LinearComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinearComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    MatSliderModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/linear/linear.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+import Chart from 'chart.js';
+
+@Component({
+  selector: 'app-linear',
+  templateUrl: './linear.component.html',
+  styleUrls: ['./linear.component.scss']
+})
+export class LinearComponent implements OnInit {
+
+  linearModel: tf.Sequential;
+  prediction: any;
+
+  myLineChart : Chart;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    var ctx = document.getElementById('myChart');
+
+    this.train();
+
+    this.myLineChart = new Chart('myChart', {
+      type: 'line',
+      data: {
+       labels: [],
+       datasets: [
+         {
+        label: 'Data',
+        fill: false,
+        data: [],
+        backgroundColor: '#168ede',
+        borderColor: '#168ede'
+         }
+       ]
+        },
+        options: {
+       tooltips: {
+        enabled: false
+       },
+       legend: {
+        display: true,
+        position: 'bottom',
+        labels: {
+         fontColor: 'white'
+        }
+       },
+       scales: {
+         yAxes: [{
+          ticks: {
+           fontColor: "white"
+          }
+         }],
+         xAxes: [{
+        ticks: {
+         fontColor: "white",
+         beginAtZero: true
+        }
+         }]
+       }
+        }
+     });
+
+    
+  }
+
+
+  async train(): Promise<any> {
+
+    // Define a model for linear regression.
+    this.linearModel = tf.sequential();
+    this.linearModel.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+    // Prepare the model for training: Specify the loss and the optimizer.
+    this.linearModel.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+
+    // Training data, completely random stuff
+    const xs = tf.tensor1d([3.2, 4.4, 5.5]);
+    const ys = tf.tensor1d([1.6, 2.7, 3.5]);
+
+
+    // Train
+    await this.linearModel.fit(xs, ys);
+
+    console.log('model trained!');
+
+
+  }
+
+  predict(value) {
+    const val = parseInt(value);
+    const output = this.linearModel.predict(tf.tensor2d([val], [1, 1])) as any;
+    this.prediction = Array.from(output.dataSync())[0];
+
+    this.myLineChart.data.labels.push(val);
+    this.myLineChart.data.datasets[0].data.push(this.prediction);
+    this.myLineChart.update();
+
+    
+    //console.log(this.data);
+  }
+
+
+}
