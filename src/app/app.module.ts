f5e6d0eb348028b07312b8397ed339e1d2268f48
FedeSchmidt <fschmidt.10@hotmail.com>
2019-10-01 20:39:51 -0300

    inicio mnist y acomodé un poco lo primero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,32 +5,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { LinearComponent } from './linear/linear.component';
-
+import { MnistComponent } from './mnist/mnist.component';
 
 const appRoutes: Routes = [
-  { path: '', component: LinearComponent },
-  { path: 'linear', component: LinearComponent },
+	{ path: '', component: LinearComponent },
+	{ path: 'linear', component: LinearComponent },
+	{ path: 'mnist', component: MnistComponent }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LinearComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot(
-      appRoutes
-    ),
-    MatSliderModule,
-    MatButtonModule,
-    MatInputModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, LinearComponent, MnistComponent ],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		RouterModule.forRoot(appRoutes),
+		MatSliderModule,
+		MatButtonModule,
+		MatInputModule
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/mnist/mnist.component.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+
+@Component({
+	selector: 'app-mnist',
+	templateUrl: './mnist.component.html',
+	styleUrls: [ './mnist.component.scss' ]
+})
+export class MnistComponent implements OnInit {
+	IMAGE_H = 28;
+	IMAGE_W = 28;
+	IMAGE_SIZE = 784;
+	NUM_CLASSES = 10;
+	NUM_DATASET_ELEMENTS = 65000;
+	NUM_TRAIN_ELEMENTS = 55000;
+	NUM_TEST_ELEMENTS = this.NUM_DATASET_ELEMENTS - this.NUM_TRAIN_ELEMENTS;
+	datasetImages: Float32Array;
+	datasetLabels: Uint8Array;
+	trainImages: any;
+	testImages: any;
+	trainLabels: any;
+	testLabels: any;
+	MNIST_IMAGES_SPRITE_PATH = 'https://storage.googleapis.com/learnjs-data/model-builder/mnist_images.png';
+	MNIST_LABELS_PATH = 'https://storage.googleapis.com/learnjs-data/model-builder/mnist_labels_uint8';
+
+	constructor() {}
+
+	ngOnInit() {}
+
+	async load() {
+		const img = new Image();
+		const canvas = document.createElement('canvas');
+		const ctx = canvas.getContext('2d');
+
+		const imgRequest = new Promise((resolve, reject) => {
+			img.crossOrigin = '';
+			img.onload = () => {
+				img.width = img.naturalWidth;
+				img.height = img.naturalHeight;
+
+				const datasetBytesBuffer = new ArrayBuffer(this.NUM_DATASET_ELEMENTS * this.IMAGE_SIZE * 4);
+
+				const chunkSize = 5000;
+				canvas.width = img.width;
+				canvas.height = chunkSize;
+
+				for (let i = 0; i < this.NUM_DATASET_ELEMENTS / chunkSize; i++) {
+					const datasetBytesView = new Float32Array(
+						datasetBytesBuffer,
+						i * this.IMAGE_SIZE * chunkSize * 4,
+						this.IMAGE_SIZE * chunkSize
+					);
+					ctx.drawImage(img, 0, i * chunkSize, img.width, chunkSize, 0, 0, img.width, chunkSize);
+
+					const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+					for (let j = 0; j < imageData.data.length / 4; j++) {
+						// All channels hold an equal value since the image is grayscale, so
+						// just read the red channel.
+						datasetBytesView[j] = imageData.data[j * 4] / 255;
+					}
+				}
+				this.datasetImages = new Float32Array(datasetBytesBuffer);
+
+				resolve();
+			};
+			img.src = this.MNIST_IMAGES_SPRITE_PATH;
+		});
+
+		const labelsRequest = fetch(this.MNIST_LABELS_PATH);
+		const [ imgResponse, labelsResponse ] = await Promise.all([ imgRequest, labelsRequest ]);
+
+		this.datasetLabels = new Uint8Array(await labelsResponse.arrayBuffer());
+
+		// Slice the the images and labels into train and test sets.
+		this.trainImages = this.datasetImages.slice(0, this.IMAGE_SIZE * this.NUM_TRAIN_ELEMENTS);
+		this.testImages = this.datasetImages.slice(this.IMAGE_SIZE * this.NUM_TRAIN_ELEMENTS);
+		this.trainLabels = this.datasetLabels.slice(0, this.NUM_CLASSES * this.NUM_TRAIN_ELEMENTS);
+		this.testLabels = this.datasetLabels.slice(this.NUM_CLASSES * this.NUM_TRAIN_ELEMENTS);
+	}
+
+	getTrainData() {
+		const xs = tf.tensor4d(this.trainImages, [
+			this.trainImages.length / this.IMAGE_SIZE,
+			this.IMAGE_H,
+			this.IMAGE_W,
+			1
+		]);
+		const labels = tf.tensor2d(this.trainLabels, [ this.trainLabels.length / this.NUM_CLASSES, this.NUM_CLASSES ]);
+		return { xs, labels };
+	}
+
+	getTestData(numExamples) {
+		let xs = tf.tensor4d(this.testImages, [
+			this.testImages.length / this.IMAGE_SIZE,
+			this.IMAGE_H,
+			this.IMAGE_W,
+			1
+		]);
+		let labels = tf.tensor2d(this.testLabels, [ this.testLabels.length / this.NUM_CLASSES, this.NUM_CLASSES ]);
+
+		if (numExamples != null) {
+			xs = xs.slice([ 0, 0, 0, 0 ], [ numExamples, this.IMAGE_H, this.IMAGE_W, 1 ]);
+			labels = labels.slice([ 0, 0 ], [ numExamples, this.NUM_CLASSES ]);
+		}
+		return { xs, labels };
+	}
+}
